13184b7c54c331b84c3edff59d8651e260a91ec7
Mycicle <mjm.digregorio@gmail.com>
2021-01-05 15:22:10 -0500

    main: writing out the example code on my own to get more familiar with dart, we also got smite API access so thats cool. Will take a look at the interface
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- smite_app/lib/main.dart
+++ smite_app/lib/main.dart
@@ -1,126 +1,190 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(SmiteApp());
 
-class MyApp extends StatelessWidget {
+
+class SmiteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: "Startup Name Generator",
+      title: "Smite Companion App",
       theme: ThemeData(
-        primaryColor: Colors.deepPurple,
+        primaryColor: Colors.white,
       ),
-      home: RandomWords(),
+      home: ScrollList(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class ScrollList extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _ScrollListState createState() => _ScrollListState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = Set<WordPair>();
-  final _biggerFont = TextStyle(fontSize: 18.0);
+class _ScrollListState extends State<ScrollList> {
+
+  final _smiteBuildSuggestions = <WordPair>[];
+  final _savedBuilds = Set<WordPair>();
+  final _biggerFont = TextStyle(fontFamily: "helvetica", fontSize: 18.0);
 
   Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
+    final _alreadySaved = _savedBuilds.contains(pair);
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      }
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: Icon(
+          _alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: _alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (_alreadySaved) {
+              _savedBuilds.remove(pair);
+            } else {
+              _savedBuilds.add(pair);
+            }
+          });
+        }
     );
   }
-
-  Widget _buildSuggestions() {
+    // ignore: dead_code
+  Widget _buildSmiteBuildSuggestions() {
     return ListView.builder(
-      padding: EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(18.0),
       itemBuilder: (context, i) {
         if (i.isOdd) return Divider();
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
+          // ignore: missing_return
+          final _index = i ~/ 2; //thats the division operator
+          if (_smiteBuildSuggestions.length <= _index) {
+            final _newBuilds = _smiteBuildSuggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_smiteBuildSuggestions[_index]);
+        },
     );
   }
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-              (WordPair pair) {
-                return ListTile(
-                  title: Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  ),
-                );
-              }
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text("Saved Suggestions"),
-            ),
-            body: ListView(children: divided),
-          );
-        }
-      ),
-    );
-  }
-
-  @override
+  void _pushBuildSuggestions() {}
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Startup Name Generator"),
+        title: Text("Stateful title"),
         actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+          IconButton(icon: Icon(Icons.list), onPressed: _pushBuildSuggestions),
         ],
       ),
-      body: _buildSuggestions(),
+      body: _buildSmiteBuildSuggestions(),
     );
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final wordPair = WordPair.random();
+//     return MaterialApp(
+//       title: "Startup Name Generator",
+//       theme: ThemeData(
+//         primaryColor: Colors.deepPurple,
+//       ),
+//       home: RandomWords(),
+//     );
+//   }
+// }
+//
+// class RandomWords extends StatefulWidget {
+//   @override
+//   _RandomWordsState createState() => _RandomWordsState();
+// }
+//
+// class _RandomWordsState extends State<RandomWords> {
+//   final _suggestions = <WordPair>[];
+//   final _saved = Set<WordPair>();
+//   final _biggerFont = TextStyle(fontSize: 18.0);
+//
+//   Widget _buildRow(WordPair pair) {
+//     final alreadySaved = _saved.contains(pair);
+//     return ListTile(
+//       title: Text(
+//         pair.asPascalCase,
+//         style: _biggerFont,
+//       ),
+//       trailing: Icon(
+//         alreadySaved ? Icons.favorite : Icons.favorite_border,
+//         color: alreadySaved ? Colors.red : null,
+//       ),
+//       onTap: () {
+//         setState(() {
+//           if (alreadySaved) {
+//             _saved.remove(pair);
+//           } else {
+//             _saved.add(pair);
+//           }
+//         });
+//       }
+//     );
+//   }
+//
+//   Widget _buildSuggestions() {
+//     return ListView.builder(
+//       padding: EdgeInsets.all(16.0),
+//       itemBuilder: (context, i) {
+//         if (i.isOdd) return Divider();
+//
+//         final index = i ~/ 2;
+//         if (index >= _suggestions.length) {
+//           _suggestions.addAll(generateWordPairs().take(10));
+//         }
+//         return _buildRow(_suggestions[index]);
+//       },
+//     );
+//   }
+//
+//   void _pushSaved() {
+//     Navigator.of(context).push(
+//       MaterialPageRoute<void>(
+//         builder: (BuildContext context) {
+//           final tiles = _saved.map(
+//               (WordPair pair) {
+//                 return ListTile(
+//                   title: Text(
+//                     pair.asPascalCase,
+//                     style: _biggerFont,
+//                   ),
+//                 );
+//               }
+//           );
+//           final divided = ListTile.divideTiles(
+//             context: context,
+//             tiles: tiles,
+//           ).toList();
+//
+//           return Scaffold(
+//             appBar: AppBar(
+//               title: Text("Saved Suggestions"),
+//             ),
+//             body: ListView(children: divided),
+//           );
+//         }
+//       ),
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Startup Name Generator"),
+//         actions: [
+//           IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+//         ],
+//       ),
+//       body: _buildSuggestions(),
+//     );
+//   }
+// }
 
 
 // import 'package:flutter/material.dart';
