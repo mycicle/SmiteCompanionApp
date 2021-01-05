d97e0263454551df6f326470d1d1774353290b27
Mycicle <mjm.digregorio@gmail.com>
2021-01-05 17:14:35 -0500

    main: Added start of smite builds homepage and named route navigation widget_test: Now uses the proper main widget name for tests
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

---
+++ smite_app/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(SmiteApp());
+
+class SmiteApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Smite Builds App",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: SmiteAppHomepage(),
+      initialRoute: "/",
+      routes: {
+        // go to the home screen
+        '/Gods': (context) => SmiteAppHomepage(),
+        '/Categories': (context) => SmiteAppHomepage(),
+        '/Items': (context) => SmiteAppHomepage(),
+        '/MyBuilds': (context) => SmiteAppHomepage(),
+        '/NewBuild': (context) => SmiteAppHomepage(),
+        // '/Gods': (context) => GodsPage(),
+        // '/Categories': (context) => CategoriesPage(),
+        // '/Items': (context) => ItemsPage(),
+        // '/MyBuilds': (context) => MyBuildsPage(),
+        // '/NewBuild': (context) => NewBuildPage(),
+      }
+    );
+  }
+}
+
+void navToPage() {
+  Text("string");
+}
+
+class SmiteAppHomepage extends StatefulWidget {
+  @override
+  _SmiteAppHomepageState createState() => _SmiteAppHomepageState();
+}
+
+class _SmiteAppHomepageState extends State<SmiteAppHomepage> {
+
+  Widget _buildMenuLink(String name) {
+    final _menuLinkFont = TextStyle(fontSize: 18.0);
+    return ListTile(
+      title: Text(
+        name,
+        style: _menuLinkFont
+      ),
+      trailing: Icon(
+          Icons.arrow_forward_ios_outlined,
+          color: Colors.grey,
+      ),
+      onTap: () {
+        //navigate to proper route
+        Navigator.pushNamed(context, "/$name");
+      },
+    );
+  }
+
+  Widget _HomepageMenu() {
+    final _menuItemNames = <String>{
+      "Gods",
+      "Categories",
+      "Items",
+      "MyBuilds",
+      "NewBuild",
+    };
+
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemCount: _menuItemNames.length*2,
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+        return _buildMenuLink(_menuItemNames.elementAt(i ~/ 2));
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Smite Builds App"),
+      ),
+      body: _HomepageMenu(),
+    );
+  }
+}
+
+
