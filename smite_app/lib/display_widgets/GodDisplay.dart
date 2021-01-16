dbcd2fe6434ec87b4230ec1b8c6156eac728770a
Mycicle <mjm.digregorio@gmail.com>
2021-01-15 21:03:09 -0500

    gods display: placeholder display text
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- smite_app/lib/display_widgets/GodDisplay.dart
+++ smite_app/lib/display_widgets/GodDisplay.dart
@@ -1,6 +1,11 @@
 import "package:flutter/material.dart";
 import "package:smite_app/classes/God.dart";
 
+
+
+
+
+
 class GodDisplay extends StatefulWidget {
   God god;
 
@@ -11,6 +16,22 @@ class GodDisplay extends StatefulWidget {
 
 class _GodDisplayState extends State<GodDisplay> {
 
+  Widget GodInformation() {
+      return Text(
+        "LORE: \n" +
+        widget.god.lore + '\n\n' +
+        "ABILITY 1: \n" +
+        widget.god.ab1.name + '\n\n' +
+        "ABILITY 2: \n" +
+        widget.god.ab2.name + '\n\n' +
+        "ABILITY 3: \n" +
+        widget.god.ab3.name + '\n\n' +
+        "ABILITY 4: \n" +
+        widget.god.ab4.name + '\n\n' +
+        "PASSIVE: \n" +
+        widget.god.ab5.name
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +39,7 @@ class _GodDisplayState extends State<GodDisplay> {
       appBar: AppBar(
         title: Text(widget.god.name + " Information"),
       ),
-      body: Container(),
+      body: GodInformation(),
     );
   }
 }
